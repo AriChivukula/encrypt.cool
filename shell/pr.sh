4c82aa6e806d2f848815cb20fe60c049f2d22ab6
Ari Chivukula <ari@chivuku.la>
2018-12-21 06:57:00 -1000

    Update pr.sh

--- shell/pr.sh
+++ shell/pr.sh
@@ -3,6 +3,6 @@ set -e
 export TF_VAR_BRANCH=$TRAVIS_PULL_REQUEST_BRANCH
 export TF_VAR_LOCAL_DOMAIN=static-$TF_VAR_BRANCH.$TF_VAR_DOMAIN
 bash shell/build.sh
-terraform init -backend-config="bucket=${TF_VAR_NAME}" -backend-config="key=tfstate/PR_${TF_VAR_BRANCH}.tfstate" infra/pr
+terraform init -backend-config="bucket=${TF_VAR_DOMAIN}" -backend-config="key=tfstate/PR_${TF_VAR_BRANCH}.tfstate" infra/pr
 terraform apply -auto-approve infra/pr
 curl -u "${GITHUB_USER}:${GITHUB_TOKEN}" -X POST -d "{\"body\": \"[Test ${TRAVIS_BUILD_NUMBER}](https://static-${TF_VAR_BRANCH}.${TF_VAR_DOMAIN})\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
