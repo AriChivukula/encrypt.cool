cca8ffabb0f0920ab50970178044175758f7c76f
Ari Chivukula <ari@chivuku.la>
2018-12-21 06:29:10 -1000

    Update trigger.sh

--- shell/trigger.sh
+++ shell/trigger.sh
@@ -1,5 +1,4 @@
 export TF_VAR_BRANCH=$TRAVIS_BRANCH
-yarn foia-db --compile
 yarn ts-node node_modules/.bin/typescriptase --files gen/*.ts
 yarn relay-compiler --src src/ --schema src/server/schema.graphql --language typescript
 terraform init -backend-config="bucket=${TF_VAR_NAME}" -backend-config="key=tfstate/trigger.tfstate" infra/trigger
