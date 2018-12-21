815b02b9ce8095ac314534df21af5ce0eb8c1378
Ari Chivukula <ari@chivuku.la>
2018-12-21 06:39:29 -1000

    Update TopBar.tsx

--- src/website/views/TopBar.tsx
+++ src/website/views/TopBar.tsx
@@ -49,33 +49,15 @@ class __TopBar extends React.Component<ITopBarProps> {
   public render(
   ): JSX.Element {
     /* BESPOKE START <<render>> */
-    const urlParts: URL = new URL(window.location.href);
-    if (urlParts.searchParams.has("code")) {
-      this.login(urlParts.searchParams.get("code") as string);
-
-      return <div />;
-    }
-    let actionItem: object = {
-      onClick: (): void => this.googleAuth(),
-      icon: "person",
-    };
-    if (this.props.data.me !== null) {
-      actionItem = {
-        onClick: (): void => this.logout(),
-        icon: "exit_to_app",
-      };
-    }
-
     return (
       <>
         <SimpleTopAppBar
           title={document.title}
           fixed={true}
           navigationIcon={{
-            onClick: (): void => goto("https://github.com/arichiv/opinionated.baby/"),
+            onClick: (): void => goto("https://github.com/AriChivukula/encrypt.cool/"),
             icon: "code",
           }}
-          actionItems={[actionItem]}
         />
         <TopAppBarFixedAdjust />
       </>
@@ -84,79 +66,6 @@ class __TopBar extends React.Component<ITopBarProps> {
   }
 
   /* BESPOKE START <<implementation>> */
-  private googleAuth(
-  ): void {
-    if ("loginURL" in this.props.data) {
-      goto(this.props.data.loginURL, true);
-    }
-  }
-
-  private login(
-    code: string,
-  ): void {
-    commitMutation(
-      // @ts-ignore
-      this.props.relay.environment,
-      {
-        mutation: graphql`
-          mutation TopBarLoginMutation($input: LoginInput) {
-            login(input: $input) {
-              accessToken
-            }
-          }
-        `,
-        onCompleted: (response: TopBarLoginMutationResponse, errors: Error[]): void => {
-          cookie.set(
-            "accessToken",
-            response.login.accessToken,
-            {
-              domain: ".opinionated.baby",
-              secure: true,
-            },
-          );
-          goto("/", true);
-        },
-        variables: {
-          input: {
-            code,
-          },
-        },
-      },
-    );
-  }
-
-  private logout(
-  ): void {
-    commitMutation(
-      // @ts-ignore
-      this.props.relay.environment,
-      {
-        mutation: graphql`
-          mutation TopBarLogoutMutation($input: LogoutInput) {
-            logout(input: $input) {
-              accessToken
-            }
-          }
-        `,
-        onCompleted: (response: TopBarLogoutMutationResponse, errors: Error[]): void => {
-          cookie.set(
-            "accessToken",
-            response.logout.accessToken,
-            {
-              domain: ".opinionated.baby",
-              secure: true,
-            },
-          );
-          goto("/", true);
-        },
-        variables: {
-          input: {
-            dummy: "",
-          },
-        },
-      },
-    );
-  }
   /* BESPOKE END <<implementation>> */
 }
 
@@ -165,10 +74,8 @@ const _TopBar: React.ComponentType<MappedFragmentProps<RemoveRelayProp<ITopBarPr
   /* BESPOKE START <<relay>> */
   graphql`
     fragment TopBarQuery on Query {
-      loginURL,
       me {
-        id,
-        email
+        id
       }
     }
   `,
