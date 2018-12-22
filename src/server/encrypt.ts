e81cc9e57fc89cc7a8ffd4e8d65d7d424bb051e3
Ari Chivukula <ari@chivuku.la>
2018-12-22 10:30:44 -0800

    Update encrypt.ts

--- src/server/encrypt.ts
+++ src/server/encrypt.ts
@@ -23,21 +23,34 @@ export type ECMetaData = {
   version: number;
 }
 
+const HMAC_PASSWORD = "0123456789ABCDEF";
+
 export function encryptContent(hint: string, message: string, password: string, ip: string): ECMetaData {
   const data = {
     created: (new Date()).toUTCString(),
     ip,
     message,
   };
-  const encrypter = Encrypter(password);
   const metaData = {
-    data: encrypter.encrypt(data),
+    data: Encrypter(password).encrypt(data),
     hash: "",
     hint,
     version: 0,
   };
-  metaData.hash = encrypter.hmac(JSON.stringify(metaData));
+  metaData.hash = Encrypter(HMAC_PASSWORD).hmac(JSON.stringify(metaData));
   return metaData;
+}
+
+export function decryptContent(metaData: ECMetaData, password: string): ECData {
+  if metaData.version !== 0 {
+    throw new Error("BAD_VERSION");
+  }
+  const hash = metaData.hash;
+  metaData.hash = "";
+  if Encrypter(HMAC_PASSWORD).hmac(JSON.stringify(metaData)) !== hash {
+    throw new Error("BAD_HASH");
+  }
+  return Encrypter(password).decrypt(metaData.data)
 }
 
 /* BESPOKE END <<custom>> */
