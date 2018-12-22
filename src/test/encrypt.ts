0c9d95ed5da37b9ead50f761782c40ea7ee86acc
Ari Chivukula <ari@chivuku.la>
2018-12-22 10:22:33 -0800

    Update encrypt.ts

--- src/test/encrypt.ts
+++ src/test/encrypt.ts
@@ -19,10 +19,11 @@ import {
 it(
   "encryptContent",
   async (): Promise<void> => {
-    const metaData = encryptContent("HINT", "MESSAGE", "PASSWORDPASSWORDPASSWORDPASSWORD", "192.168.0.1")
+    const metaData = encryptContent("HINT", "MESSAGE", "PASSWORDPASSWORDPASSWORDPASSWORD", "192.168.0.1");
+    chai.expect(metaData.data).to.equal("d2323d29e3f460a525c9827d9fdc0fbc1eab896857c22a2f952277504be55a9e");
+    chai.expect(metaData.hash).to.equal("d2323d29e3f460a525c9827d9fdc0fbc1eab896857c22a2f952277504be55a9e");
     chai.expect(metaData.hint).to.equal("HINT");
     chai.expect(metaData.version).to.equal(0);
-    chai.expect(metaData.hash).to.equal("");
   },
 );
 /* BESPOKE END <<custom>> */
