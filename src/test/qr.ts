f45da99e55c7c6fb078fc736f91da2f8affb728c
Ari Chivukula <ari@chivuku.la>
2018-12-22 10:21:47 -1000

    Update qr.ts

--- src/test/qr.ts
+++ src/test/qr.ts
@@ -7,4 +7,33 @@
  */
 
 /* BESPOKE START <<custom>> */
+import "mocha";
+
+import * as chai from "chai";
+
+import {
+  encodeQR,
+  decodeQR,
+} from "../server/qr";
+
+it(
+  "encodeQR",
+  async (): Promise<void> => {
+    const qr = encodeQR("HINT", "MESSAGE", "PASSWORDPASSWORDPASSWORDPASSWORD", "192.168.0.1");
+    chai.expect(qr).to.not.be.empty;
+  },
+);
+
+it(
+  "decodeQR",
+  async (): Promise<void> => {
+    const password = "PASSWORDPASSWORDPASSWORDPASSWORD";
+    const metaData = await encryptContent("HINT", "MESSAGE", password, "192.168.0.1");
+    const url = URI_PREFIX + encodeURIComponent(JSON.stringify(metaData));
+    const data = decodeQR(url, password);
+    chai.expect(data.created).to.not.be.empty;
+    chai.expect(data.ip).to.equal("192.168.0.1");
+    chai.expect(data.message).to.equal("MESSAGE");
+  },
+);
 /* BESPOKE END <<custom>> */
