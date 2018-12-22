b7146df35dd2a0f459776d4dd9be9e9be66df9ea
Ari Chivukula <ari@chivuku.la>
2018-12-22 10:04:32 -0800

    Update encrypt.ts

--- src/server/encrypt.ts
+++ src/server/encrypt.ts
@@ -7,4 +7,44 @@
  */
 
 /* BESPOKE START <<custom>> */
+
+import Encrypter from "simple-encryptor";
+import {
+  Request,
+} from "express";
+import {
+  getClientIp,
+} from "request-ip";
+
+export type ECData = {
+  created: string;
+  ip: string;
+  message: string;
+}
+
+export type ECMetaData = {
+  data: string;
+  hash: string;
+  hint: string;
+  version: number;
+}
+
+export function encryptContent(hint: string, message: string, password: string, request: Request): ECMetaData {
+  const data = {
+    created: (new Date()).toUTCString(),
+    ip: getClientIp(request)
+    message,
+  };
+  const encrypter = Encrypter(password);
+  const metaData = {
+    data: encrypter.encrypt(data),
+    hash: "",
+    hint,
+    version: 0,
+  };
+  const hash = encryptor.hmac(metaData);
+  metaData.hash = hash;
+  return metaData;
+}
+
 /* BESPOKE END <<custom>> */
