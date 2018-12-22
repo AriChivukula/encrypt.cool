76fcb4386e7971a9aa054821f86f3d3d5b9d8312
Travis CI User <travis@example.org>
2018-12-22 19:06:00 +0000

    ACTUALIZE qr-lib

---
+++ src/test/qr.ts
@@ -0,0 +1,10 @@
+/**
+ * This file is partially generated; only edit bespoke sections.
+ *
+ * SOURCE<<gen/test.ts::qr>>
+ * BESPOKE<<custom>>
+ * SIGNED<<J9FCy5Eb9w0afcgAaRFGr3gxQY8sbFhMkYnvR4PLOLGutLf1jQN3nT49Pgi/UgT8KqpaBJanNZoyEo3inVMTZQ==>>
+ */
+
+/* BESPOKE START <<custom>> */
+/* BESPOKE END <<custom>> */
