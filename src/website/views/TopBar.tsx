b99aa47933b94353c34540a374dfd8778e205673
Ari Chivukula <ari@chivuku.la>
2019-06-26 08:53:02 -0700

    Update TopBar.tsx

--- src/website/views/TopBar.tsx
+++ src/website/views/TopBar.tsx
@@ -1,17 +1,14 @@
-/**
- * This file is partially generated; only edit bespoke sections.
- *
- * SOURCE<<gen/website.ts::TopBar>>
- * BESPOKE<<imports, render, implementation>>
- * SIGNED<<kHq6yzfWokPWNCMfURWlsAo7OaNhM+PwwwMO6Te6t8SJLQwaTPwqNT1Wxn/7796cljUiKz+Kte+BHsjRUX9YnQ==>>
- */
-
 import * as React from "react";
 
-/* BESPOKE START <<imports>> */
 import * as cookie from "js-cookie";
 
-import TopAppBar, {TopAppBarFixedAdjust} from "@material/react-top-app-bar";
+import TopAppBar, {
+  TopAppBarFixedAdjust,
+  TopAppBarIcon,
+  TopAppBarRow,
+  TopAppBarSection,
+  TopAppBarTitle,
+} from "@material/react-top-app-bar";
 import MaterialIcon from "@material/react-material-icon";
 import {
   Url,
@@ -20,7 +17,6 @@ import {
 import {
   goto,
 } from "../utility";
-/* BESPOKE END <<imports>> */
 
 export interface ITopBarProps {
 }
@@ -29,25 +25,25 @@ class _TopBar extends React.Component<ITopBarProps> {
 
   public render(
   ): JSX.Element {
-    /* BESPOKE START <<render>> */
     return (
       <>
-        <TopAppBar
-          title={document.title}
-          fixed={true}
-          navigationIcon={<MaterialIcon
-            onClick={(): void => goto("https://github.com/AriChivukula/encrypt.cool/")}
-            icon="code"
-          />}
-        />
+        <TopAppBar fixed>
+          <TopAppBarRow>
+            <TopAppBarSection align='start'>
+              <TopAppBarIcon navIcon tabIndex={0}>
+                <MaterialIcon
+                  onClick={(): void => goto("https://github.com/AriChivukula/encrypt.cool/")}
+                  icon="code"
+                />
+              </TopAppBarIcon>
+              <TopAppBarTitle>{document.title}</TopAppBarTitle>
+            </TopAppBarSection>
+          </TopAppBarRow>
+        <TopAppBar />
         <TopAppBarFixedAdjust />
       </>
     );
-    /* BESPOKE END <<render>> */
   }
-
-  /* BESPOKE START <<implementation>> */
-  /* BESPOKE END <<implementation>> */
 }
 
 export { _TopBar as TopBar };
