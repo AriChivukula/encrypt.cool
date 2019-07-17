85e1f66e1edef22aaa553fc116115d3c390ec644
Ari Chivukula <ari@chivuku.la>
2019-07-17 15:11:58 -0700

    Update root.ts

--- src/server/root.ts
+++ src/server/root.ts
@@ -1,13 +1,7 @@
-/**
- * This file is partially generated; only edit bespoke sections.
- *
- * SOURCE<<gen/server.ts::root>>
- * BESPOKE<<custom>>
- * SIGNED<<J9FCy5Eb9w0afcgAaRFGr3gxQY8sbFhMkYnvR4PLOLGutLf1jQN3nT49Pgi/UgT8KqpaBJanNZoyEo3inVMTZQ==>>
- */
-
-/* BESPOKE START <<custom>> */
-import * as express from "express";
+import {
+  IncomingMessage,
+  ServerResponse,
+} from "http";
 import {
   getClientIp,
 } from "request-ip";
@@ -21,8 +15,8 @@ import {
 } from "./utility";
 
 export async function genRoot(
-  req: express.Request,
-  res: express.Response,
+  req: IncomingMessage,
+  res: ServerResponse,
 ): Promise<object> {
   return {
     dummy: async (): Promise<{ id: number }> => {
@@ -38,4 +32,3 @@ export async function genRoot(
     },
   };
 }
-/* BESPOKE END <<custom>> */
